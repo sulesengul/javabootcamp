f1c20988aa4c39f75e64807bf7135b9aa5db1dbd
sulesengul <114023332+sulesengul@users.noreply.github.com>
2022-09-20 23:48:38 +0300

    Ödev1

---
+++ Ödev1/degisken/src/degisken/Main.java
@@ -0,0 +1,13 @@
+package degisken;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int sayi = 12;
+		String mesaj ="Öğrenci sayısı : ";
+		System.out.println(mesaj + sayi);
+		System.out.println(mesaj + sayi);
+		System.out.println("Öğrenci sayım : "+ sayi);
+		System.out.println("Öğrenci sayım : "+ sayi);
+	}
+}
---
+++ Ödev1/mukemmelSayi/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author CANSU
+ *
+ */
+module mukemmelSayi {
+}
